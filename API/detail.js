9b6bdf43bc6a9f96b54629cb64dd2ca9362ed67b
chun1hao <270063757@qq.com>
2020-05-14 22:10:19 +0800

    Update detail.js

--- API/detail.js
+++ API/detail.js
@@ -1,14 +1,14 @@
 // 1.instanceof
 function myInstanceof(left, right){
-  if(typeof left !== 'object' || left === null) return false;
-  let proto = Object.getPrototypeOf(left);
-  while(true){
-    //  查找到最顶层
-    if(proto === null) return false;
-    if(proto === right.prototype) return true;
-    //  再向上查找
-    proto = Object.getPrototypeOf(proto);
-  }
+    if(typeof left !== 'object' || left === null) return false;
+    let proto = Object.getPrototypeOf(left);
+    while(true){
+        //  查找到最顶层
+        if(proto === null) return false;
+        if(proto === right.prototype) return true;
+        //  再向上查找
+        proto = Object.getPrototypeOf(proto);
+    }
 }
 
 // 2.Object.is()
