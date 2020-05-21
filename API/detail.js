b7034d835ed3ee6c13c4ecb6068e9845b42ab90c
chun1hao <270063757@qq.com>
2020-05-21 22:58:21 +0800

    Update detail.js

--- API/detail.js
+++ API/detail.js
@@ -43,3 +43,19 @@ function myFlat(arr){
     // }
     // return arr
 }
+
+// 4. call
+function myCall(context) {
+    var context = context || window;
+    context.fn = this;
+
+    var args = [];
+    for(var i = 1, len = arguments.length; i < len; i++) {
+        args.push('arguments[' + i + ']');
+    }
+
+    var result = eval('context.fn(' + args +')');
+
+    delete context.fn
+    return result;
+}
