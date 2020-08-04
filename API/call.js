f7bc984fd91073dc9701e71c2ca2383a8867b358
chun1hao <270063757@qq.com>
2020-08-04 15:01:33 +0800

    Update call.js

--- API/call.js
+++ API/call.js
@@ -55,6 +55,7 @@ Function.prototype.myBind = function (context) {
         // 一个绑定函数也能使用new操作符创建对象：这种行为就像把原函数当成构造器。提供的 this 值被忽略，同时调用时的参数被提供给模拟函数。
         return self.apply(this instanceof temFn ? this : context, args.concat(...arguments)) // 函数有返回值
     }
+    // 修改返回值的 prototype 为绑定的 prototype ，继承绑定函数的原型中的值
     temFn.prototype = this.prototype
     fn.prototype = new temFn()
     return fn
