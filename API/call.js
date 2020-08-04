531731ea617c99a136aa02c3faac0117b60b475c
chun1hao <270063757@qq.com>
2020-08-04 14:28:04 +0800

    Update call.js

--- API/call.js
+++ API/call.js
@@ -20,6 +20,24 @@ Function.prototype.myApply = function (context, arr) {
     delete context.fn
     return result;
 }
+Function.prototype.myApply = function (context, arr) {
+    var obj = params || window, res, symKey = 'kk' + Math.random(),len = arguments.length
+    while(obj.hasOwnProperty(symKey)){
+        symKey = 'kk' + Math.random()
+    }
+    obj[symKey] = this
+    var str = ''
+    for(var i=1;i<len;i++){
+        str += arguments[i] + (i == len -1 ? '' : ',')
+    }
+    if(len>1){        
+        res = eval('obj[symKey]('+str+')')
+    }else{
+        res = obj[symKey]()
+    }
+    delete obj[symKey]
+    return res
+}
 
 // 3.bind 
 // bind() 方法会创建一个新函数。当这个新函数被调用时，bind() 的第一个参数将作为它运行时的 this，之后的一序列参数将会在传递的实参前传入作为它的参数。
